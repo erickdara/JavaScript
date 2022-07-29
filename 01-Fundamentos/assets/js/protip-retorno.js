e933e700b309b1007b14140e1f93ecd881ae814a
Erick David Rangel Garcia <erick1rg@gmail.com>
2022-07-28 23:36:27 -0500

    Pro Tips retorno
    Pro Tips retorno

--- 01-Fundamentos/assets/js/protip-retorno.js
+++ 01-Fundamentos/assets/js/protip-retorno.js
@@ -19,9 +19,14 @@ const imprimeArgumentos2 = (edad, ...args) => {
     return args;
 }
 
+// Con esta sintaxis se puede retornar las constantes declaradas en el arreglo 
+// en cada indice con su valor correspondiente al argumento pasados a la función
 const [casado, vivo, nombre, saludo] = imprimeArgumentos2(10,true,false, 'Fernando', 'Hola');
 console.log({casado, vivo, nombre, saludo});
 
+// Con esta sintaxis se puede retornar un objeto especifico
+// del objeto retornado como argumento en la función usando la declaración
+// const {nombre: nuevoNombreDeseado}
 const { apellido: nuevoApellido } = crearPersona ('Erick', 'Rangel');
 console.log({nuevoApellido});
 
